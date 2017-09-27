560086f5b23195e6aedb9c246143a0a9ab7450e0
ggi <ggi@mydebian2>
2017-09-27 15:53:34 +0200

    ajout de la pièce la plus importante

--- main.py
+++ main.py
@@ -11,15 +11,13 @@ BLUE = (0,0,200)
 BLACK = (0,0,0)
 #End color
 #Map Texture
-GROUND = (58,137,35)
-PLAYER = (236, 31, 30)
-ENERGY_CELL = (255, 250, 98)
+GROUND = (0, 0, 0)
 #End Map texture
 
 #Coord
 POS_X = 0
 POS_Y = 90
-TILE_SIZE = 40 #Toujours egal a player_size -> juste nom plus evident 
+TILE_SIZE = 50 #Toujours egal a player_size -> juste nom plus evident 
 LOOKING = 0 # (left = 0, up = 1, right = 2, down = 3)
 #End Coord
 
@@ -53,9 +51,16 @@ def drawMap(pygame, data):
     display = pygame.display.set_mode((map_size*TILE_SIZE,map_size*TILE_SIZE))
     for row in range(map_size):
         for column in range(map_size):
-            pygame.draw.rect(display, tile_map[row][column], (column*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            if tile_map[row][column] == GROUND:
+                pygame.draw.rect(display, tile_map[row][column], (column*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            elif tile_map[row][column] == PLAYER:
+                display.blit(PLAYER, (column*TILE_SIZE, row*TILE_SIZE))
+            else:
+                display.blit(ENERGY_CELL, (column*TILE_SIZE, row*TILE_SIZE))
 
 pygame.init()
+PLAYER = pygame.image.load("bunny50.png")
+ENERGY_CELL = pygame.image.load("energy_max50.png")
 clock = pygame.time.Clock()
 socket = myZmqFunc.connectToPubServer("12345")
 run = True
@@ -63,10 +68,15 @@ while run:
     notification = convertToJson(myZmqFunc.getNotification(socket))
     if notification['notification_type'] == 0:
         drawMap(pygame, notification['data'])
-        
-    #for row in range(MAP_SIZE):
-    #    for column in range(MAP_SIZE):
-    #        pygame.draw.rect(DISPLAY, tilemap[row][column], (column*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+    elif notification['notification_type'] == 1:
+        print("En attente de joueurs")
+    elif notification['notification_type'] == 2:
+        print("Un seul client est encore en vie")
+    elif notification['notification_type'] == 3:
+        print("Mort d'un client")
+    elif notification['notification_type'] == 4:
+        print("Victoire d'un client")
+
     #player = pygame.transform.rotate(image_player, direction)
     #display.blit(player, (pos_x, pos_y))
     pygame.display.update()
